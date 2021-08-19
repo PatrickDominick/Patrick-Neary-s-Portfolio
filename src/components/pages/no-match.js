11767509f00c0a97209c481cdab9819304f34f22
PatrickDominick <pdneary@gmail.com>
2021-08-19 18:07:51 -0500

    End of 2nd day

---
+++ src/components/pages/no-match.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+
+export default function() {
+    return (
+        <div>
+            <h2>Return to sender. Address unknown</h2>
+            <Link to = "/">Return to hompage sucka!</Link>
+        </div>
+    )
+}
