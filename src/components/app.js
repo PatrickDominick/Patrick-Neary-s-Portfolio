d8ae44933fdc5c595131af59112579ed3de0466b
PatrickDominick <pdneary@gmail.com>
2021-08-20 18:02:46 -0500

    styled up!

--- src/components/app.js
+++ src/components/app.js
@@ -1,7 +1,5 @@
 import React, { Component } from "react";
-import moment from "moment";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-
 
 import PortfolioContainer from "./portfolio/portfolio-container";
 import NavigationContainer from "./navigation/navigation-container";
@@ -10,6 +8,7 @@ import About from "./pages/about";
 import Contact from "./pages/contact";
 import Blog from "./pages/blog";
 import PortfolioDetail from "./portfolio/portfolio-detail";
+import Auth from "./pages/auth";
 import NoMatch from "./pages/no-match";
 
 export default class App extends Component {
@@ -19,15 +18,15 @@ export default class App extends Component {
   render() {
     
     return (
-      <div className="app">
+      <div className="container">
         <Router>
           <div>
-            <h1>Patrick Neary's Portfolio</h1>
-            <div>{moment().format("MMMM Do YYYY, h:mm:ss a")}</div>
+            
             <NavigationContainer />
 
             <Switch>
               <Route exact path="/" component={Home} />
+              <Route path="/auth" component={Auth} />
               <Route path="/about-me" component={About} />
               <Route path="/contact" component={Contact} />
               <Route path="/blog" component={Blog} />
